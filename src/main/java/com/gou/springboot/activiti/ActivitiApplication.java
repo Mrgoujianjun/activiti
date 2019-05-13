dfda8a3c78e8f45a0c6159647676b59c8c503a82
lygoujianjun <lygoujianjun@163.com>
2019-05-13 10:02:39 +0800

    搭建工程

---
+++ src/main/java/com/gou/springboot/activiti/ActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.gou.springboot.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class, args);
+    }
+
+}
